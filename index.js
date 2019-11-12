79471d598a8945a7a4baa25795596dee83e035f5
Kristin Baday <kristinsbdy@gmail.com>
2019-11-12 17:15:45 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,3 @@
+console.error("This is a stern warning that a mistake just happened");
+console.warn("This a slight touch that you are in the wrong direction");
+console.log("This is just a standard output to the user");
